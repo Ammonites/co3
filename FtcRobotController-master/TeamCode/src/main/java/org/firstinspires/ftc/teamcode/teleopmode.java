30fe772e25228e104b31bc38b9279927a1207045
Ammonites <j110319@my.cmsh.cyc.edu.tw>
2024-12-30 19:10:22 +0800

    init

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopmode.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopmode.java
@@ -4,75 +4,27 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name = "TeleOpMode")
-class MyTeleOpMode extends LinearOpMode  {
-    public class TeleOpMode extends TeleOp_Base {
-        double drive, turn, strafe;
-        double fr, fl, br, bl, scale;
-        @Override
-        public void runOpMode() throws InterruptedException {
-            init_hardware();
-            waitForStart();
-            while(opModeIsActive()) {
-                drive = -gamepad1.left_stick_y;     //前進
-                turn = gamepad1.right_stick_x;      //自旋
-                strafe = gamepad1.left_stick_x;    //平移
+@TeleOp(name ="TeleOpMode")
+public class teleopmode extends LinearOpMode{
+    //變數設定
 
-
-                fr = drive + turn - strafe;
-                fl = drive - turn + strafe;
-                br = drive + turn + strafe;
-                bl = drive - turn - strafe;
-
-
-                scale = scaling_power(fr, fl, br, bl);
-
-
-                FR.setPower(fr/scale);
-                FL.setPower(fl/scale);
-                BR.setPower(br/scale);
-                BL.setPower(bl/scale);
-            }
-        }
-
-        private double scaling_power(double fr, double fl, double br, double bl) {
-            return fr;
-        }
-    }
-    private DcMotorEx leftMotor;
-    private DcMotorEx rightMotor;
-
+    //創建物件
     @Override
     public void runOpMode() throws InterruptedException {
         init_hardware();
         waitForStart();
+        //初始狀態設定，例如Servo初始位置
+        while(opModeIsActive()) {
+            //迴圈執行內容
 
-        while (opModeIsActive()) {
-            double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
-
-            double leftPower = drive + turn;
-            double rightPower = drive - turn;
-
-            leftMotor.setPower(leftPower);
-            rightMotor.setPower(rightPower);
-
-            telemetry.addData("Left Power", leftPower);
-            telemetry.addData("Right Power", rightPower);
-            telemetry.update();
             idle();
         }
     }
-
+    // 建立函式
     private void init_hardware() {
-        leftMotor = hardwareMap.get(DcMotorEx.class, "left_motor");
-        rightMotor = hardwareMap.get(DcMotorEx.class, "right_motor");
-
-        leftMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        rightMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-    }
-
-    private abstract class TeleOp_Base {
-        public abstract void runOpMode() throws InterruptedException;
+        //設定物件
+        
+        idle();
     }
 }
+// 外面不可以寫程式喔!!!
